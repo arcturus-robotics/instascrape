0236f0f4211bf9a2e097d51707276180b4f8efa4
jakbyte <jakbyte@gmail.com>
2019-12-21 15:55:30 -0500

    Remove Discord functionality (for now) and improve everything

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Instascrape
 
-A Discord bot to help keep track of our Instagram followers.
+A bot to help keep track of our Instagram followers.
+
+## Configuration
+
+Configuration is stored in a file named `config.toml`.
+
+To keep track of our Instagram every 6 hours, for example, you would write the following:
+
+```toml
+# https://www.instagram.com/arcturusrobotics
+user = "arcturusrobotics"
+# 21600 seconds = 6 hours
+interval = 21600
+```
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,125 @@
+#![deny(
+    rust_2018_idioms,
+    clippy::all,
+    missing_debug_implementations,
+    missing_copy_implementations
+)]
+
+use chrono::Utc;
+use reqwest::Client;
 use scraper::{Html, Selector};
-use serenity::{
-    client::Client,
-    framework::standard::{
-        macros::{command, group},
-        CommandResult, StandardFramework,
-    },
-    model::channel::Message,
-    prelude::*,
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+    thread,
+    time::Duration,
 };
-use std::{env, fs::File, io::Read};
 
-fn get_followers() -> u64 {
-    let user = {
-        let mut file = File::open("./user.txt").unwrap();
-        let mut buf = String::new();
-        file.read_to_string(&mut buf).unwrap();
-        buf
-    };
-
-    let content = reqwest::get(format!("https://www.instagram.com/{}", user).as_str())
-        .unwrap()
-        .text()
-        .unwrap();
-    let document = Html::parse_document(&content);
-
-    let selector = Selector::parse(r#"meta[property="og:description"]"#).unwrap();
-    let meta = document.select(&selector).next().unwrap();
-    let content = meta.value().attr("content").unwrap();
-
-    content
-        .split_terminator(' ')
-        .next()
-        .unwrap()
-        .parse::<u64>()
-        .unwrap()
+/// The scraper's configuration.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Config {
+    /// The user to fetch data from.
+    pub user: String,
+    /// The update interval in seconds.
+    pub interval: u64,
 }
 
-group!({
-    name: "general",
-    options: {},
-    commands: [followers],
-});
+impl Config {
+    pub fn new(user: &str, interval: u64) -> Self {
+        Self {
+            user: String::from(user),
+            interval,
+        }
+    }
 
-struct Handler;
-
-impl EventHandler for Handler {}
-
-fn main() {
-    let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
-        .expect("Error creating client");
-    client.with_framework(
-        StandardFramework::new()
-            .configure(|c| c.prefix("~"))
-            .group(&GENERAL_GROUP),
-    );
-
-    if let Err(why) = client.start() {
-        println!("An error occurred while running the client: {:?}", why);
+    /// Load the config.
+    pub fn load() -> Self {
+        let mut file = File::open("./config.toml").unwrap();
+        let mut buf = String::new();
+        file.read_to_string(&mut buf).unwrap();
+        toml::de::from_str(&buf).unwrap()
     }
 }
 
-#[command]
-fn followers(ctx: &mut Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, format!("{}", get_followers()))?;
+/// An Instagram scraper.
+#[derive(Debug, Clone)]
+pub struct Scraper {
+    pub client: Client,
+    pub config: Config,
+}
 
-    Ok(())
+impl Scraper {
+    pub fn url(&self) -> String {
+        format!("https://www.instagram.com/{}", self.config.user)
+    }
+
+    pub fn duration(&self) -> Duration {
+        Duration::from_secs(self.config.interval)
+    }
+
+    /// Scrape the followers from the document.
+    pub fn followers(&self) -> Option<u64> {
+        let document = self.document();
+
+        let selector = Selector::parse(r#"meta[property="og:description"]"#).unwrap();
+        let meta = match document.select(&selector).next() {
+            Some(meta) => meta,
+            None => return None,
+        };
+        let content = meta.value().attr("content").unwrap().trim();
+
+        match content.find("Followers") {
+            Some(index) => Some(match content[..index].trim().parse::<u64>() {
+                Ok(followers) => followers,
+                Err(_) => return None,
+            }),
+            None => None,
+        }
+    }
+
+    /// Run the scraper and output CSV to a file at the specified path.
+    pub fn run<P>(&self, path: P)
+    where
+        P: AsRef<Path>,
+    {
+        let duration = self.duration();
+
+        let mut file = File::create(path.as_ref()).unwrap();
+        loop {
+            let followers = match self.followers() {
+                Some(followers) => followers,
+                None => continue,
+            };
+
+            let _ = file
+                .write(format!("{},{}\n", Utc::now(), followers).as_bytes())
+                .unwrap();
+            file.flush().unwrap();
+
+            thread::sleep(duration);
+        }
+    }
+
+    fn document(&self) -> Html {
+        Html::parse_document(
+            &self
+                .client
+                .get(self.url().as_str())
+                .send()
+                .unwrap()
+                .text()
+                .unwrap(),
+        )
+    }
+}
+
+fn main() {
+    let scraper = Scraper {
+        client: Client::new(),
+        config: Config::load(),
+    };
+
+    scraper.run("./followers.csv");
 }
